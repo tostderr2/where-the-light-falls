5afd82f673d4c795048c99d587f6c6bca3e251c6
tostderr2 <tostderr@gmail.com>
2026-09-08 19:49:15 +0530

    Init: setup of the project with build system working with initial depencies

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <glm/common.hpp>
+#include <glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
+#include "core/Log.h"
+
+
+int main(/*int argc, char *agrv[]*/)
+{
+	spdlog::log(spdlog::level::level_enum::info, "Welcome to he greatest game ever created\n");
+	return 0;
+}
